4c2c9223c7b60faf7f4b167bf419d43633a5909d
martyncoup <Martyn.Coupland@transparity.com>
2022-12-14 14:33:47 +0000

    Initial commit

---
+++ AzParse/Resources.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace AzParse;
+
+public static class Resources
+{
+    public static string ToFriendlyName(this string value)
+    {
+        return Resources.ResourceMap.TryGetValue(value, out var displayName) ? displayName : value;
+    }
+
+    public static Dictionary<string, string> ResourceMap = new()
+	{
+		{ "Microsoft.Compute/virtualMachines", "Virtual Machine" }
+	};
+}
